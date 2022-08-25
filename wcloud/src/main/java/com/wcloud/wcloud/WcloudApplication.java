e8a62071383f634daa805a9b3e3990e97578d62a
joonasp <56842740+Joonasp1@users.noreply.github.com>
2022-08-25 11:47:52 +0300

    initial commit

---
+++ wcloud/src/main/java/com/wcloud/wcloud/WcloudApplication.java
@@ -0,0 +1,13 @@
+package com.wcloud.wcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcloudApplication.class, args);
+	}
+
+}
